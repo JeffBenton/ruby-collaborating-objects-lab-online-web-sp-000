bf33ae2b16c546a333d61816b091e659a167b35d
Jeff Benton <sikletz@gmail.com>
2019-02-11 23:52:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,7 @@ class Song
   def self.new_by_filename(path)
     data = path.split(" - ")
     song = Song.new(data[1])
-    song.artist = data[0]
+    song.artist = Artist.find_or_create_by_name(data[0])
     song.genre = data[2].chomp(".mp3")
     song
   end
