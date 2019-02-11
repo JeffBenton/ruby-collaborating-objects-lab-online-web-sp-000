909c8da54092c39887a7a6bd08283ef369458cec
Jeff Benton <sikletz@gmail.com>
2019-02-11 23:45:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,16 @@
 class Song
   attr_accessor :name
+  @@all = []
   def initialize(name)
     @name = name
+    @@all << self
+  end
+  
+  def self.new_by_filename(path)
+    
+  end
+  
+  def self.all
+    @@all
   end
 end
